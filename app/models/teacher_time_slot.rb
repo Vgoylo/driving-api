ba4c8f18c5cc2d59ff1e5eafbb6e688eee299466
Vitali Hoila <vgoylo91mav@gmail.com>
2024-03-27 21:42:55 +0300

    Add teacher time slot model

---
+++ app/models/teacher_time_slot.rb
@@ -0,0 +1,3 @@
+class TeacherTimeSlot < ApplicationRecord
+  belongs_to :teacher
+end
---
+++ test/models/teacher_time_slot_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class TeacherTimeSlotTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
